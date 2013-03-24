466f4236354c47671b1a8bce116669e008f45df6
vladimir <protsenkovi@gmail.com>
2013-03-24 20:04:09 +0400

    mend
    Fixed issue with unuploaded ThreshApply.java changes.

--- buildrun.sh
+++ buildrun.sh
@@ -5,8 +5,9 @@ echo $DIR
 ant
 
 sudo -u hduser /usr/local/hadoop/bin/hadoop dfs -rmr /user/hduser/method-otsu-in
+sudo -u hduser /usr/local/hadoop/bin/hadoop dfs -rmr /user/hduser/tmp
 sudo -u hduser /usr/local/hadoop/bin/hadoop dfs -rmr /user/hduser/method-otsu-out
 
-sudo -u hduser /usr/local/hadoop/bin/hadoop dfs -copyFromLocal $DIR/input /user/hduser/in
+sudo -u hduser /usr/local/hadoop/bin/hadoop dfs -copyFromLocal $DIR/input /user/hduser/method-otsu-in
 
-sudo -u hduser /usr/local/hadoop/bin/hadoop jar $DIR/MethodOzuTest.jar /user/hduser/in /user/hduser/method-otsu-out
+sudo -u hduser /usr/local/hadoop/bin/hadoop jar $DIR/MethodOzuTest.jar /user/hduser/method-otsu-in /user/hduser/method-otsu-out
